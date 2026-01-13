ddbae1e2cb04b5aa7b815786d2e82e78b29f4c07
anasselghazi <anasseelghazi10@gmail.com>
2026-01-13 20:42:58 +0100

    feat:ajouter la class abstarct de BaseModel

---
+++ app/models/BaseModel.php
@@ -0,0 +1,10 @@
+<?php
+abstract class BaseModel {
+    protected $pdo ;
+    protected function __construct(PDO $pdo){
+        $this->pdo=$pdo;
+    }
+    abstract public function save(): bool;
+    abstract public static function find(int $id);
+}
+?>
